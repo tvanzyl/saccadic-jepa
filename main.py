b0b6dfe214e126472c6e0abd96843468783cfa40
tvanzyl <tvanzyl@gmail.com>
2026-04-19 08:20:37 +0200

    better wandb

--- main.py
+++ main.py
@@ -323,7 +323,7 @@ def pretrain(
     # Train model.
     metric_callback = MetricCallback()
     logger = TensorBoardLogger(save_dir=str(log_dir), name="pretrain")    
-    wandb_logger = WandbLogger(save_dir=str(log_dir), name="js-repa", prefix=method)
+    wandb_logger = WandbLogger(save_dir=str(log_dir), group=method, job_type="pretrain")
     wandb_logger.experiment.config["batch_size"] = batch_size_per_device
     trainer = Trainer(
         max_epochs=epochs,
